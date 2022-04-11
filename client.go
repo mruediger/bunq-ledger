4c5d594053a7b1dd7a9e64e2777c9eecd68d9de8
Mathias Rüdiger <ruediger@blueboot.org>
2022-04-11 11:57:07 +0200

    start refactoring out of tests

--- client.go
+++ client.go
@@ -1,12 +1,17 @@
 package bunq_ledger
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"net/url"
 )
 
 type Client struct {
-	URL url.URL
+	URL         url.URL
+	private_key rsa.PrivateKey
 }
 
 func NewClient(address string) (*Client, error) {
@@ -15,9 +20,33 @@ func NewClient(address string) (*Client, error) {
 		return nil, fmt.Errorf("bunq-ledger: could not parse entpoint address %q: %w", address, err)
 	}
 
+	private_key, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, fmt.Errorf("bunq-ledger: could not create the keypair %w", err)
+	}
+
 	c := &Client{
-		URL: *addressURL,
+		URL:         *addressURL,
+		private_key: *private_key,
 	}
 
 	return c, nil
+}
+
+func (c *Client) postInstallation() error {
+	url := c.URL
+	url.Path = "/v1/installation"
+
+	pubKey, err := x509.MarshalPKIXPublicKey(c.private_key.Public())
+	if err != nil {
+		return fmt.Errorf("foo")
+	}
+
+	pubKeyPemBlock := pem.Block{
+		Type:    "PUBLIC KEY",
+		Headers: nil,
+		Bytes:   pubKey,
+	}
+
+	return nil
 }
